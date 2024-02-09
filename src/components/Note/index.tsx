7f36a9e5199f82387ce0531a0b31055ef020e69e
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-02-09 13:01:40 -0300

    Create notes with specific sizes

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './style.css';
 
-const Note = () => {
+interface NoteInterface {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+const Note = ({ x = 0, y = 0, w = 0, h = 0, }: NoteInterface) => {
+  const noteRef = useRef<any>(null);
   const [drag, setOnDrag] = useState(false);
   const [position, setPosition] = useState({
     deltaX: 0,
     deltaY: 0,
-    x: '0px',
-    y: '0px'
+    x: `${x}px`,
+    y: `${y}px`
   });
+
+  useEffect(() => {
+    const note = noteRef.current;
+    note.style.left = position.x;
+    note.style.top = position.y;
+    note.style.width = `${w}px`;
+    note.style.height = `${h}px`;
+  }, []);
 
   const handleMouseDown = (e :any) => {
     setOnDrag(true);
@@ -31,8 +47,9 @@ const Note = () => {
     if (drag) {
       const x = `${e.clientX - position.deltaX}px`;
       const y = `${e.clientY - position.deltaY}px`;
-      e.target.style.left = x;
-      e.target.style.top = y;
+      const note = noteRef.current;
+      note.style.left = x;
+      note.style.top = y;
 
       setPosition({...position, x, y });
     }
@@ -49,8 +66,12 @@ const Note = () => {
         e.stopPropagation();
       }}
       className="note"
+      ref={noteRef}
     >
-      <textarea onClick={e => e.stopPropagation() } />
+      <textarea
+        placeholder="Type something you don't want to forget later"
+        onClick={e => e.stopPropagation() }
+        />
     </div>
   );
 }
--- src/components/NotePad/index.tsx
+++ src/components/NotePad/index.tsx
@@ -2,12 +2,30 @@ import { useState, ReactElement } from 'react';
 import Note from '../Note';
 import './style.css';
 
+const noteSizes = {
+  small: { width: 130, height: 150 },
+  medium: { width: 280, height: 300 },
+  large: { width: 430, height: 450 },
+}
+
 const Notepad = () => {
   const [notes, setNotes] = useState<ReactElement[]>([]);
+  const [noteSize, setNoteSize] = useState(noteSizes.medium);
 
-  const handleDoublClick = () => {
-    const note = <Note key={new Date().getTime()} />;
+  const handleDoublClick = (e: any) => {
+    const key = new Date().getTime();
+    const note = <Note key={key} x={e.clientX} y={e.clientY} w={noteSize.width} h={noteSize.height} />;
     setNotes([...notes, note]);
+  }
+
+  const SizeSelect = () => {
+    return (
+      <select onChange={e => setNoteSize(noteSizes[e.target.value])}>
+        <option value="small">Small</option>
+        <option selected value="medium">Medium</option>
+        <option value="large">Large</option>
+      </select>
+    )
   }
 
   return (
@@ -16,7 +34,7 @@ const Notepad = () => {
       onDoubleClick={handleDoublClick}
     >
       <h1>
-        Double click anywhere to create a new note
+        Double click anywhere to create a <SizeSelect /> note
       </h1>
       {
         notes.map(note => note)
